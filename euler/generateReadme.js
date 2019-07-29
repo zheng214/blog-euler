0241faed6685a0983dc64c585fa0e82c46992a5f
Zheng <zheng.n.shang@hotmail.com>
2019-07-28 20:21:57 -0400

    1

--- euler/generateReadme.js
+++ euler/generateReadme.js
@@ -18,7 +18,7 @@ stream.once('open', async (fd) => {
   stream.write('\n\n<br/><br/>\n\n');
   stream.write('## <p align="center"> Solutions </p>');
   stream.write('\n');
-  stream.write('**Problem** | **Description** | **Solution** \n');
+  stream.write('**Problem** | **Description** | **Solution**         \n');
   stream.write('------------|-----------------|------------- \n');
   await generateMenu();
   stream.write('\n\n');
