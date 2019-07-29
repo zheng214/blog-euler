725bae0a48ca9fff3e36b4bccc2916713ebf31ea
Zheng <zheng.n.shang@hotmail.com>
2019-07-28 20:26:53 -0400

    1

--- euler/generateReadme.js
+++ euler/generateReadme.js
@@ -65,7 +65,7 @@ function generateFileMenu(file) {
         // matches the question statement
         const questionMatch = line.match(/@question (.+)$/);
         if (questionMatch) {
-          questionCapture += ` ${questionMatch[1]}<br/>`;
+          questionCapture += ` ${questionMatch[1]}`;
         }
 
         // matches the start of the solution
