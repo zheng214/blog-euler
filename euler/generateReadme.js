877d5610129203dadbd1e7644c5e033bb90af7db
Zheng <zheng.n.shang@hotmail.com>
2019-09-17 23:19:28 -0400

    54

--- euler/generateReadme.js
+++ euler/generateReadme.js
@@ -129,7 +129,9 @@ function generateFileMenu(file) {
             const eulerURL = eulerURLTemplate
               .replace('{problem}', problemID);
 
-            const breakProblemQuestion = problemDetails.length && problemDetails[problemDetails.length - 1] ? '<br/><br/>' : '';
+            const breakProblemQuestion = problemDetails.length
+              ? problemDetails[problemDetails.length - 1] ? '<br/><br/>' : '<br/>' 
+              : '';
             console.log({ problemID, breakProblemQuestion })
             stream.write(`**${problemID}.** [${problemName}](${eulerURL}) | `);
             stream.write(`${problemDetails.join('<br/>')}${breakProblemQuestion}${questionLines.join('<br/>')} | `);
