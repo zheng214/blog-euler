2da30b9f29fbfc81a5f4c50ddcf67348394ab624
Zheng <zheng.n.shang@hotmail.com>
2019-09-23 22:37:09 -0400

    54

--- euler/generateReadme.js
+++ euler/generateReadme.js
@@ -12,7 +12,7 @@ stream.once('open', async (fd) => {
   stream.write('## <p align="center"> Project Euler Menu </p>');
   stream.write('\n\n');
   stream.write('This repository contains my solutions to the problems of Project Euler ');
-  stream.write('(<a href="https://projecteuler.net/about" target="_blank">Official website</a> / <a href="https://en.wikipedia.org/wiki/Project_Euler" target="_blank">Wikipedia</a>)');
+  stream.write('(<a href="https://projecteuler.net/about"">Official website</a> / <a href="https://en.wikipedia.org/wiki/Project_Euler">Wikipedia</a>)');
   stream.write('\n\n');
   stream.write('Each numbered folder contains 10 solutions');
   stream.write('\n\n');
