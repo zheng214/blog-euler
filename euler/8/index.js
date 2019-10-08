5c084c8595188978d6d44a3479af8ce16e15c37b
Zheng <zheng.n.shang@hotmail.com>
2019-10-07 22:12:47 -0400

    74

--- euler/8/index.js
+++ euler/8/index.js
@@ -9,7 +9,7 @@ module.exports = {
    * 1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3, 5/7, 3/4, 4/5, 5/6, 6/7, 7/8
    *
    * It can be seen that 2/5 is the fraction immediately to the left of 3/7.
-   * @question By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order of size, find the numerator of the fraction immediately to the left of 3/7.
+   * @question By listing the set of reduced proper fractions for d ≤ 1,000,000 in ascending order, find the numerator of the fraction immediately to the left of 3/7.
    */
   e71() {
     // for each d <= 1,000,000, we just need to find maximal integer c such that c/d is smaller than 3/7
