3474ac2b85ee7fbf4959c59b2daa5441e598c6f3
Zheng <zheng.n.shang@hotmail.com>
2019-10-10 19:29:23 -0400

    81

--- euler/9/index.js
+++ euler/9/index.js
@@ -5,11 +5,11 @@ module.exports = {
    * In the 5 by 5 matrix below, the minimal path sum from the top left to the bottom right, by only moving to the right and down,
    * is indicated in bold and is equal to 2427.
    *
-   * **131** **673** 234 103 18
-   * 201 **96** **342** 965 150
+   * **131** **673** 234 103 018
+   * 201 **096** **342** 965 150
    * 630 803 **746** **422** 111
    * 537 699 497 **121** 956
-   * 805 732 524 **37** **331**
+   * 805 732 524 **037** **331**
    *
    * @question Find the minimal path sum, in matrix.txt, a text file containing a 80 by 80 matrix, from the top left to the bottom right by only moving right and down.
    */
