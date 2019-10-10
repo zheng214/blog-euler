45723820e2fc69342774a0513298620fd9bafcec
Zheng <zheng.n.shang@hotmail.com>
2019-10-09 20:03:31 -0400

    79

--- euler/8/index.js
+++ euler/8/index.js
@@ -337,7 +337,7 @@ module.exports = {
   },
 
   /**
-   * Problem 78
+   * Problem 78 Coin Partitions
    *
    * Let p(n) represent the number of different ways in which n coins can be separated into piles.
    * For example, five coins can be separated into piles in exactly seven different ways, so p(5)=7.
