50c9f78cd5c4b6cc78b0fd24df307413d453dc0b
Zheng <zheng.n.shang@hotmail.com>
2019-10-21 19:40:21 -0400

    web init

---
+++ build/precache-manifest.79eb5aac9761dbfafc33a5c66eb2c1ee.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3054c864b1a4cad060e063682014df5",
+    "url": "/euler/index.html"
+  },
+  {
+    "revision": "b36e8aa3f532cb265748",
+    "url": "/euler/static/js/main.0f156d60.chunk.js"
+  },
+  {
+    "revision": "5885a0f26491c2c4b137",
+    "url": "/euler/static/js/runtime-main.ad0ee135.js"
+  }
+]);
